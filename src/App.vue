c0c403f8f6e755028262c7c7f3f31ddc1db5782a
EmmanuelAVT9 <alexsoge@gmail.com>
2024-10-15 14:07:48 -0600

    feat: Create component MyCounterScript2 separate script in file ts

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
     <h1>Mi primera app</h1>
     <hr>
-    <MyCounter :value="5" />
-    <MyCounterScript :value="5" />
+    <MyCounter :value="1" />
+    <MyCounterScript :value="2" />
+    <MyCounterScript2 :value="3" />
 </template>
 
 <script setup lang="ts">
 import MyCounter from './components/MyCounter.vue';
 import MyCounterScript from './components/MyCounterScript.vue';
+import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue';
+
 </script>
--- src/components/my-counter-script/MyCounterScript2.ts
+++ src/components/my-counter-script/MyCounterScript2.ts
@@ -1,16 +1,3 @@
-<template>
-    <section>
-        <h3> Counter: {{ counter }}</h3>
-        <h3> Square: {{ squareCounter }}</h3>
-
-        <div>
-            <button @click="counter++">+1</button>
-            <button @click="counter--">-1</button>
-        </div>
-    </section>
-</template>
-
-<script lang="ts">
 import { computed, defineComponent, ref } from "vue";
 
 export default defineComponent({
@@ -27,4 +14,3 @@ export default defineComponent({
         }
     },
 })
-</script>
