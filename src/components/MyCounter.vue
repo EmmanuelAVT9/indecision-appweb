b6d036736c9703a0bc22e03e43b223d12f90340b
EmmanuelAVT9 <alexsoge@gmail.com>
2024-10-15 15:09:26 -0600

    feat: Add TailwindCSS framework CSS and configuration

--- src/components/MyCounter.vue
+++ src/components/MyCounter.vue
@@ -3,8 +3,8 @@
         <h3> Counter: {{ counter }}</h3>
         <h3> Square: {{ squareCounter }}</h3>
         <div>
-            <button class="btn btn-primary" @click="counter++">+1</button>
-            <button @click="counter--">-1</button>
+            <button class="p-5 bg-blue-500 rounded hover:bg-blue-700 mr-2" @click="counter++">+1</button>
+            <button class="p-5 bg-blue-500 rounded hover:bg-blue-700" @click="counter--">-1</button>
         </div>
     </section>
 </template>
