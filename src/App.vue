b0bb13eff8c8980b7fd2035fa5e4b1b63fd186de
EmmanuelAVT9 <alexsoge@gmail.com>
2024-10-15 13:20:12 -0600

    feat: Create componente MyCounterScript for view diference in script setup and no setup

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
     <h1>Mi primera app</h1>
     <hr>
-    <MyCounter :value="5" text="Soy un texto" />
+    <MyCounter :value="5" />
+    <MyCounterScript :value="5" />
 </template>
 
 <script setup lang="ts">
 import MyCounter from './components/MyCounter.vue';
+import MyCounterScript from './components/MyCounterScript.vue';
 </script>
--- src/components/MyCounter.vue
+++ src/components/MyCounter.vue
@@ -2,7 +2,6 @@
     <section>
         <h3> Counter: {{ counter }}</h3>
         <h3> Square: {{ squareCounter }}</h3>
-        <h4>{{ text }}</h4>
         <div>
             <button @click="counter++">+1</button>
             <button @click="counter--">-1</button>
@@ -16,7 +15,6 @@ import { computed, ref } from 'vue';
 // Interfaz para las propiedades, tiene una propiedad 'value' de tipo número y propiedad 'text' de tipo string
 interface Props {
     value: number;
-    text: string;
 }
 
 // Define las propiedades del componente usando la interfaz
